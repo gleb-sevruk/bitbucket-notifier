becd344db02d1a5105d6bec69165fd8a6995c354
Gleb Sevruk <glebs@checkpoint.com>
2025-03-17 20:01:34 +0200

    Added logging + update lock file

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,16 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use tauri::Manager;
 use crate::commands::update_dock_badge_safe;
+use crate::logging::setup_logging;
+use log::info;
 
 mod commands;
-
-
+mod logging;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    setup_logging();
+    info!("Starting app  🤬 🤯 😳 🥵 🥶 😱 😨 ");
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_notification::init())
